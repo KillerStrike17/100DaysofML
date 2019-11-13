783ba8b5c27caefa19098fe73ed05893c000fb4a
KillerStrike17 <shubham.agnihotri1997@gmail.com>
2019-11-13 14:41:32 +0530

    Update on twitter API

--- Natural Language Processing/Stream_Twitter/twitter_stream.py
+++ Natural Language Processing/Stream_Twitter/twitter_stream.py
@@ -113,7 +113,6 @@ class Clean_Data():
 		try:
 			df = pd.DataFrame()
 			for friend in friendlist:
-				print(dir(friend))
 				df = df.append([[friend.name,friend.screen_name]])
 			df.columns = ['Name','Screen_Name']
 			return df
@@ -379,22 +378,3 @@ class Exception_Handler():
 			# To sleep on RateLimit Exceed Error
 			except tweepy.RateLimitError:
 				time.sleep(15 * 60)
-
-
-if __name__ == "__main__":
-	# hashed_list = ['Trump','USA']
-	# twitter_streamer = Stream_Tweets()
-	# twitter_streamer.stream_data(hashed_list)
-
-	twitter_client = User_Tweets()
-	
-	tweets = twitter_client.get_user_timeline_tweet(10,"killerstrike17")
-	
-	friendlist = twitter_client.get_friendlist_of_user(10)
-	# print(friendlist)
-	clean_data = Clean_Data()
-	# df = clean_data.friends_to_df(friendlist)
-	df = clean_data.tweets_to_df(tweets)
-
-	print(df)
-	
