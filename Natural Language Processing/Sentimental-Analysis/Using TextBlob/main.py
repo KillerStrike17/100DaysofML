f15fc17f82fbb47a2efbb0184f17a9d6410b6122
KillerStrike17 <shubham.agnihotri1997@gmail.com>
2019-11-15 18:23:42 +0530

    update

--- Natural Language Processing/Sentimental-Analysis/Using TextBlob/main.py
+++ Natural Language Processing/Sentimental-Analysis/Using TextBlob/main.py
@@ -5,7 +5,15 @@ from twitter_stream import *
 from sentiment_analysis import *
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+def check(sentiment):
+    #print(sentiment)
+    if sentiment == "Positive":
+        return 1
+    elif sentiment == "Negative":
+        return -1
+    else:
+        return 0
 
 # Stream of Hashed words
 # hashed_list = ['Trump','USA']
@@ -15,17 +23,35 @@ import numpy as np
 # Stream of Users Tweets
 a = Analyze_Tweet()
 
-# twitter_client = User_Tweets()
-# tweets = twitter_client.get_user_timeline_tweet(10,"realDonaldTrump")
-# clean_data = Clean_Data()
-# clean_data.tweets_to_df(tweets)
+twitter_client = User_Tweets()
+tweets = twitter_client.get_user_timeline_tweet(10,"killerstrike17")
+clean_data = Clean_Data()
+clean_data.tweets_to_df(tweets)
 df = pd.read_csv('user_tweets.csv')
 # for index, row in df.iterrows() :
 #     print(a.Analyze_Sentiment(row[0]))
 df['sentiments'] = np.array([a.Analyze_Sentiment(row[0]) for index,row in df.iterrows()])
-print(df.columns)
-df2 = df.filter(['Tweet','sentiments'],axis = 1)
-print(df2)
+#print(df.columns)
+df2 = df.filter(['sentiments'],axis = 1)
+#print(df2)
+counter_variable = 0
+y = []
+# df2['sentiments_label'] = np.array([counter_variable  = counter_variable + check(row[0]) for index,row in df2.iterrows()])
+for index,row in df2.iterrows():
+    counter_variable  = counter_variable + check(row[0])
+    y.append(counter_variable)
+
+print(len(df2))
+x= range(len(df2))
+#y = df2['sentiments_label']
+plt.plot(x,y)
+plt.show()
+#np.array([check(row[0]) 
+# for index,row in df2.iterrows():
+#     print(check(row[0]))
+# ax = plt.gca()
+# df.plot()
+
 
 
 # # Stream of User Friendlist Tweets
--- Natural Language Processing/Stream_Twitter/twitter_credentials.py
+++ Natural Language Processing/Stream_Twitter/twitter_credentials.py
@@ -1,9 +1,9 @@
 #variables for Twitter Credentials
 
 # Consumer API Keys
-consumer_key="YOUR KEY"
-consumer_secret="YOUR KEY"
+consumer_key="5zVB669nyLpWBLyWxYEEMoPxU"
+consumer_secret="W7Wd0BKn8iwNhth9y1F90bV3VycfESP8DJ9TWMWXRFqgUKHBvi"
 
 # Access Token and Access Token Secret Kets
-access_token="YOUR KEY"
-access_secret="YOUR KEY"
+access_token="982495000247316480-AczUG27Zz11k6nj6hynZFhhjLwfQ1gn"
+access_secret="aUkRBYo1QM9LG0G5EErm3nWYGO4HOd1wNDd2LTVgSv5mO"
