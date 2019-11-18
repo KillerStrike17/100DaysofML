c573ee2bb30617c4e855e1c98a48b43bc4d04d29
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2019-11-18 16:48:37 +0530

    Update main.py

--- Natural Language Processing/Sentimental-Analysis/Using TextBlob/main.py
+++ Natural Language Processing/Sentimental-Analysis/Using TextBlob/main.py
@@ -6,6 +6,15 @@ from sentiment_analysis import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# ###
+# Function to convert Sentiments to values
+# 
+# Parameter:
+# ---------
+# 	Sentiment -  it is a variable that contains sentiment of a text. 
+# ###
 def check(sentiment):
     #print(sentiment)
     if sentiment == "Positive":
@@ -15,42 +24,29 @@ def check(sentiment):
     else:
         return 0
 
-# Stream of Hashed words
-# hashed_list = ['Trump','USA']
-# twitter_streamer = Stream_Tweets()
-# twitter_streamer.stream_data(hashed_list)
-
 # Stream of Users Tweets
-a = Analyze_Tweet()
-
 twitter_client = User_Tweets()
 tweets = twitter_client.get_user_timeline_tweet(2000,"realDonaldTrump")
 clean_data = Clean_Data()
 clean_data.tweets_to_df(tweets)
 df = pd.read_csv('user_tweets.csv')
-# for index, row in df.iterrows() :
-#     print(a.Analyze_Sentiment(row[0]))
+
+# Analyzing Sentiments
+a = Analyze_Tweet()
 df['sentiments'] = np.array([a.Analyze_Sentiment(row[0]) for index,row in df.iterrows()])
-#print(df.columns)
 df2 = df.filter(['sentiments'],axis = 1)
-#print(df2)
+
+# Plotting Graph
 counter_variable = 0
 y = []
-# df2['sentiments_label'] = np.array([counter_variable  = counter_variable + check(row[0]) for index,row in df2.iterrows()])
 for index,row in df2.iterrows():
     counter_variable  = counter_variable + check(row[0])
     y.append(counter_variable)
 
 print(len(df2))
-x= range(len(df2))
-#y = df2['sentiments_label']
+x = range(len(df2))
 plt.plot(x,y)
 plt.show()
-#np.array([check(row[0]) 
-# for index,row in df2.iterrows():
-#     print(check(row[0]))
-# ax = plt.gca()
-# df.plot()
 
 
 
@@ -58,19 +54,12 @@ plt.show()
 # friendlist = twitter_client.get_friendlist_of_user(10,"killerstrike17")
 # clean_data.friends_to_df(friendlist)
 
-# Sentimental Analysis of Batches 
-
-# Live Plotting of Sentiments
-
-#
 # twitter_client = User_Tweets()
 	
-# tweets = twitter_client.get_user_timeline_tweet(10,"killerstrike17")
+# tweets = twitter_client.get_user_timeline_tweet(100,"realDonaldTrump")
 
 # friendlist = twitter_client.get_friendlist_of_user(10)
 # # print(friendlist)
 # clean_data = Clean_Data()
 # df = clean_data.friends_to_df(friendlist)
 # #df = clean_data.tweets_to_df(tweets)
-
-# print(df)
